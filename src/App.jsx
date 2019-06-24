003e897c27716db46d3ac49595fb834fd4cc8afb
unknown <leena.sonar@successive.tech>
2019-06-24 23:20:18 +0530

    save

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+//import { TextFieldDemo } from './pages';
+//import { Slider } from './components'
+import  InputDemo  from './pages/Inputdemo/Inputdemo';
+
+function App() {
+    return <InputDemo />;
+}
+
+
+export default App;
+
+
+/*import { TextFieldDemo}from "./pages";
+
+const App = () => TextFieldDemo();
+
+export default App;*/
